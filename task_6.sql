88fff1eb21fa29b5cb1a9f3496eb3db99cef81b5
Yonatan <desisayonatan77@gmail.com>
2024-08-02 02:28:34 -0700

    task_5

---
+++ task_6.sql
@@ -0,0 +1,8 @@
+-- Switch to the alx_book_store database
+USE alx_book_store;
+
+-- Insert multiple rows into the Customers table
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', "124 Happiness  Ave."),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', "125 Happiness  Ave."),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', "126 Happiness  Ave.");
