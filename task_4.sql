3b7cf5e1c03c8a7e2a831e4bd0e6915bcd3291c2
Yonatan <desisayonatan77@gmail.com>
2024-08-02 02:25:13 -0700

    task_4

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+-- Switch to the alx_book_store database
+USE alx_book_store;
+
+-- Query to get full description of the Books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
